06ff3159d8fe35ecf92177eb66f4da33ebd0a99e
JackH01 <jackhicks01@outlook.com>
2025-05-28 16:24:59 +0100

    Created restapi using routers to access product_categories and products

--- backend/stock_tracker/views.py
+++ backend/stock_tracker/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ProductCategorySerializer, ProductSerializer
+from .models import ProductCategory, Product
 
 # Create your views here.
+class ProductCategoryView(viewsets.ModelViewSet):
+    serializer_class = ProductCategorySerializer
+    queryset = ProductCategory.objects.all()
+
+class ProductView(viewsets.ModelViewSet):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+

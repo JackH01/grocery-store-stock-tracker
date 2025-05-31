44dbd780dc4616333d662a48bf98172199896bae
JackH01 <jackhicks01@outlook.com>
2025-05-31 11:07:09 +0100

    Made category name unique

--- backend/stock_tracker/models.py
+++ backend/stock_tracker/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class ProductCategory(models.Model):
-    name = models.CharField(max_length=120)
+    name = models.CharField(max_length=120, unique=True)
 
 class Product(models.Model):
     name = models.CharField(max_length=120)
